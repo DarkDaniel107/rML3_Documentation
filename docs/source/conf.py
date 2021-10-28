8e87a1f10d43b398620588e5cbd89df813293157
DarkDaniel107 <67437547+DarkDaniel107@users.noreply.github.com>
2021-10-28 13:38:10 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'rML3'
+copyright = '2021, QuantumEdge'
+author = 'DarkDaniel107'
 
 release = '0.1'
 version = '0.1.0'
